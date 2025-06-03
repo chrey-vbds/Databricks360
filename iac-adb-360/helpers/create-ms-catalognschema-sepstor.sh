613b4ab338ec31de39385e35247f5f8c24f9e732
chrey-gh <chrey@microsoft.com>
2025-06-03 16:57:46 -0700

    changed var env to ENV

--- iac-adb-360/helpers/create-ms-catalognschema-sepstor.sh
+++ iac-adb-360/helpers/create-ms-catalognschema-sepstor.sh
@@ -3,24 +3,24 @@
 
 # parameters
 resourcegroupname=$1 #'rg-wus3-adbmidp0912-dev'
-echo "$resourcegroupname as parm 1"
+echo "Resourcegroup: $resourcegroupname as parm 1"
 tenantid=$2 #'12ce7121-18c7-4841-98f9-3b26fc8af34f'
-echo "$tenantid as parm 2"
+echo "Tenant: $tenantid as parm 2"
 # client-id
 clientid=$3 #'a439677f-074f-4dbe-9af3-b9f39fb74ba0'
-echo "$clientid as parm 3"
+echo "Clientid: $clientid as parm 3"
 clientsecret=$4 #'<<none>>'
-echo "$clientsecret as parm 4"
+echo "Secret: $clientsecret as parm 4"
 metastorename=$5
 echo "metastorename: $metastorename as parm 5"
 env=$6
 echo "environment: $env as parm 6"
 storageaccountname=$7
-echo "$storageaccountname as parm 7"
+echo "Catalogstorage: $storageaccountname as parm 7"
 credname=$8
-echo "$credname as parm 8"
+echo "CredentialName: $credname as parm 8"
 accessconnectorid=$9
-echo "$accessconnectorid as parm 9"
+echo "AccessconnectorID: $accessconnectorid as parm 9"
 
 # variables
 extlocationname="catextloc$env"
